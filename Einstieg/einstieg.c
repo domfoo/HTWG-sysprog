eee1f4d8816ae8b5c7da92ab396cb9885ca94ee0
Dominik Bartsch <dominik.bartsch@gmx.net>
2022-12-15 10:58:36 +0100

    add solution for Einstieg

---
+++ Einstieg/einstieg.c
@@ -0,0 +1,25 @@
+/*
+* einstieg.c
+*
+* Das erste C-Programm gibt einen Text aus.
+* Author: Dominik Bartsch
+* Erstellt am: 24.03.2022
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    printf("Anzahl bisher geschriebener C-Programme eingeben: ");
+    int anzahl;
+    scanf("%d", &anzahl);
+
+    printf("Vorname eingeben: ");
+    char *vorname = (char*) malloc(8);
+    scanf("%s", vorname);
+
+    printf("%ss %d. C-Programm funktioniert!\n", vorname, anzahl + 1);
+
+    // free(vorname);
+    return 0;
+}
